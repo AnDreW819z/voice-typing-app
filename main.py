1d4fddc9793b214be32aa0eb934c465bfe0d6d56
MSWANDR <110108014+AnDreW819z@users.noreply.github.com>
2026-07-17 15:34:13 +0300

    Fix: suppress HuggingFace Hub console warnings

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+import warnings
+
+# Подавляем консольный спам от HuggingFace Hub при первой загрузке модели
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+os.environ["HF_HUB_DISABLE_EXPERIMENTAL_WARNING"] = "1"
+warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
+
 from app import VoiceTypingApp
 
 def main():
